4fdcdaba2634fad3b75fc439aa3952bb9d8d679b
akriti-pandey <theakriti.99@gmail.com>
2023-05-15 21:51:03 +0300

    fix

--- src/utilityLibraries/utility.py
+++ src/utilityLibraries/utility.py
@@ -7,13 +7,10 @@ import logging
 def read_urls_from_configfile(file_path):
     """
     This function reads list of http urls from the config file
-
     Args:
         file_path as string: config path as string
-    
     Return:
         urls as List: List of urls present in configuration file
-
     """
     with open(file_path) as file:
         urls = []
@@ -28,13 +25,11 @@ def verifyContentRequiement(url, content_requirements):
     """
         This function triggers GET method to collect response data for each the URL and calculates 
         the response time of url and verifies the content requirement for each url , then logs the message in the log file
-
         Args:
             urls as List: List of http URL
             content_requirements as Dictionary: Dictionary for content requiremnet where key contains URL and value
             contains string content
             interval as Literal
-
         Return: N/A
         """
     try:
@@ -54,7 +49,15 @@ def verifyContentRequiement(url, content_requirements):
         content = response.text
 
         # validate content requirement
-        if all(requirement in content for requirement in content_requirements.get(url, [])):
+        requirements = content_requirements.get(url, [])
+        dataFound = True
+
+        for data in requirements:
+            if data not in content:
+                dataFound = False
+                break
+
+        if dataFound            
             message = f'{url} is up and running. Response time: {elapsed_time:.2f}s'
             logging.info(message)
             print(message)
@@ -72,13 +75,11 @@ def verifyContentRequiement(url, content_requirements):
 def validateURLAndVerifyContentRequiement(urls, content_requirements, interval):
     """
     This function validates the URL and verifies the content requirement for each url
-
     Args:
         urls as List: List of http URL
         content_requirements as Dictionary: Dictionary for content requiremnet where key contains URL and value
         contains string content
         interval as Literal
-
     Return: N/A
     """
     for url in urls:
@@ -89,13 +90,10 @@ def validateURLAndVerifyContentRequiement(urls, content_requirements, interval):
 def configureLogging(logFileName):
     """
     This function configure the logging file .
-
     Args:
         logFileName as string
-
     Returns:
         U/A
-
     """
     logging.basicConfig(filename=logFileName, level=logging.INFO,
                         format='%(asctime)s - %(levelname)s - %(message)s')
